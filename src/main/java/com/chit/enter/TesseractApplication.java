c59903a557cd76bdc5bac96e11117a551b8cc281
Am-Coder <mishraaman2210@gmail.com>
2019-12-23 18:09:19 +0530

    "egit push"

--- src/main/java/com/chit/enter/TesseractApplication.java
+++ src/main/java/com/chit/enter/TesseractApplication.java
@@ -2,7 +2,7 @@ package com.chit.enter;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-//Tesseract App
+//Tesseract Application
 @SpringBootApplication
 public class TesseractApplication {
 
